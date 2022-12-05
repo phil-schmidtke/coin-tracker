3c1d5b9f723668a9032cf4c0382ba6121d7d870b
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-05 21:58:26 +0100

    Navbar with Animations

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,51 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function Navbar() {
     return (
-        <div className="flex justify-between p-4">
+        <motion.div initial={{ y: -50 }}
+                    animate={{ y: 0 }}
+                    transition={{ delay: 0.5 }}
+                    className="flex justify-between p-4">
             <div>
-                <h1 className="text-xl font-bold px-4 py-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Coin-Tracker</h1>
+                <motion.button whileHover={{
+                                    scale: 1.05,
+                                    transition: { duration: 0.2 },
+                                }}
+                                whileTap={{ scale: 0.9 }}
+                                className="text-xl font-bold px-4 py-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Coin-Tracker</motion.button>
             </div>
             <div className="flex">
-                <a className="px-4 py-2 mx-4 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Home</a>
-                <a className="px-4 py-2 mx-4 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Dashboard</a>
-                <a className="px-4 py-2 mx-4 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">List</a>
+                <motion.a whileHover={{
+                            scale: 1.05,
+                            transition: { duration: 0.2 },
+                        }}
+                        whileTap={{ scale: 0.9 }}
+                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Home</motion.a>
+                <motion.a whileHover={{
+                            scale: 1.05,
+                            transition: { duration: 0.2 },
+                        }}
+                        whileTap={{ scale: 0.9 }}
+                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Dashboard</motion.a>
+                <motion.a whileHover={{
+                            scale: 1.05,
+                            transition: { duration: 0.2 },
+                        }}
+                        whileTap={{ scale: 0.9 }}
+                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">List</motion.a>
             </div>
             <div>
-                Settings
+                <motion.button whileHover={{
+                            scale: 1.05,
+                            transition: { duration: 0.2 },
+                        }}
+                        whileTap={{ scale: 0.9 }}
+                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">
+                    Settings
+                </motion.button>
             </div>
-        </div>
+        </motion.div>
     )
 }
