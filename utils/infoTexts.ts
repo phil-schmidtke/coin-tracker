146eaf3b14733e9d575b038e48bd3ce7743d42be
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-12 13:50:50 +0100

    complete info text constants

--- utils/infoTexts.ts
+++ utils/infoTexts.ts
@@ -12,3 +12,38 @@ export const USDT = {
     title: 'Tether',
     text: 'Tether is a blabla bla'
 }
+
+export const BNB = {
+    title: 'Binance Coin',
+    text: 'Binance Coin is a blabla bla'
+}
+
+export const USDC = {
+    title: 'USD Coin',
+    text: 'USD Coin is a blabla bla'
+}
+
+export const BUSD = {
+    title: 'Binance USD',
+    text: 'Binance USD is a blabla bla'
+}
+
+export const XRP = {
+    title: 'XRP | Ripple',
+    text: 'Ripple is a blabla bla'
+}
+
+export const DOGE = {
+    title: 'Dogecoin',
+    text: 'Dogecoin is a blabla bla'
+}
+
+export const ADA = {
+    title: 'Cardano',
+    text: 'Cardano is a blabla bla'
+}
+
+export const MATIC = {
+    title: 'Polygon',
+    text: 'Polygon is a blabla bla'
+}
