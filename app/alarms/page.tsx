1f26afd255ffe010abb683ec0958290e265c78ac
phil.schmidtke <phil.schmidtke@telekom.de>
2022-12-13 16:31:24 +0100

    new navigation

---
+++ app/alarms/page.tsx
@@ -0,0 +1,7 @@
+export default function Alarms() {
+    return (
+        <div>
+            <h1 className="text-center text-xl font-bold">Alarms</h1>
+        </div>
+    )
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 
-export default function Navbar() {
+export default function Navbar() {    
     return (
         <motion.div initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
@@ -13,27 +14,27 @@ export default function Navbar() {
                                     transition: { duration: 0.2 },
                                 }}
                                 whileTap={{ scale: 0.9 }}
-                                className="text-xl font-bold px-4 py-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Coin-Tracker</motion.button>
+                                className="px-4 py-2 text-xl font-bold cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl"><Link href="/" className="px-4 py-2">Coin-Tracker</Link></motion.button>
             </div>
             <div className="md:flex hidden">
-                <motion.a whileHover={{
+                <motion.button whileHover={{
                             scale: 1.05,
                             transition: { duration: 0.2 },
                         }}
                         whileTap={{ scale: 0.9 }}
-                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Home</motion.a>
-                <motion.a whileHover={{
+                        className="mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl"><Link href="/" className="p-4">Home</Link></motion.button>
+                <motion.button whileHover={{
                             scale: 1.05,
                             transition: { duration: 0.2 },
                         }}
                         whileTap={{ scale: 0.9 }}
-                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">Dashboard</motion.a>
-                <motion.a whileHover={{
+                        className="mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl"><Link href="/alarms" className="p-4">Alarms</Link></motion.button>
+                <motion.button whileHover={{
                             scale: 1.05,
                             transition: { duration: 0.2 },
                         }}
                         whileTap={{ scale: 0.9 }}
-                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">List</motion.a>
+                        className="mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl"><Link href="/about" className="p-4">About</Link></motion.button>
             </div>
             <div>
                 <motion.button whileHover={{
@@ -41,8 +42,8 @@ export default function Navbar() {
                             transition: { duration: 0.2 },
                         }}
                         whileTap={{ scale: 0.9 }}
-                        className="px-4 py-2 mx-2 cursor-pointer hover:bg-neutral-800 rounded-xl hover:drop-shadow-xl">
-                    Settings
+                        className="px-4 py-2 mx-2 cursor-pointer bg-neutral-800 rounded-xl hover:shadow-2xl">
+                    Login
                 </motion.button>
             </div>
         </motion.div>
