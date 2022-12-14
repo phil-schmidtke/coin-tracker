d7396330458a6472d5c90750fe1f59fb4e9cd36f
phil.schmidtke <phil.schmidtke@telekom.de>
2022-12-14 16:44:37 +0100

    alarm page layout

--- app/alarms/page.tsx
+++ app/alarms/page.tsx
@@ -1,7 +1,19 @@
 export default function Alarms() {
     return (
         <div>
-            <h1 className="text-center text-xl font-bold">Alarms</h1>
+            <div className="flex">
+                <div className="mx-auto">
+                    <h2 className="text-2xl font-bold text-center">Create a new Alarm</h2>
+                    <p className="text-sm text-center mt-2">If the current Market price will hit your alarm price, <br />
+                    it will mint an NFT an send it to your connected Wallet. <br />
+                    This is our way of an anonomys Web3 alarm. <br /> The Image will be connected to your Alarm Settings.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
