01719d733e45df809eb69385da29dcf0a5044287
Phil Schmidtke <mail@phil-schmidtke.de>
2023-01-04 20:45:38 +0100

    Update cryptoList.tsx

--- components/cryptoList.tsx
+++ components/cryptoList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import CryptoListItem from "./CryptoListItem";
+import CryptoListItem from "./cryptoListItem";
 
 type CryptoListProps = {
     data: any;
